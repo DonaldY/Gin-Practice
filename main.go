11dd522e84ba50e6c534a5041cdb42101107d361
Donald <448641125@qq.com>
2019-05-07 17:15:23 +0800

    update nil

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
 
@@ -17,11 +22,35 @@ func main() {
 		})
 	})
 
+	type EnergyPair struct {
+		Energy uint   `json:"energy"`
+		Profit uint   `json:"profit"`
+	}
+
 	r.GET("/ping2", func(context *gin.Context) {
 
+		energyPair := EnergyPair{nil, nil}
+
 		context.JSON(200, gin.H{
-			"username": "DONALD",
-			"password": "123",
+			"username": nil,
+			"password": nil,
+			"energy": energyPair,
+		})
+	})
+
+	r.GET("/token", func(context *gin.Context) {
+
+		resp, err := http.Get("http://localhost:8080/token")
+		if err != nil {
+			panic(err)
+
+		}
+		defer resp.Body.Close()
+		s, err:=ioutil.ReadAll(resp.Body)
+		fmt.Printf(string(s))
+
+		context.JSON(200, gin.H{
+			"token": s,
 		})
 	})
 
